9486457fbf58037d0b2b97e923a12202e9bced5e
RathanakSeng <40135662+RathanakSeng@users.noreply.github.com>
2018-10-08 22:52:56 +0700

    Semester

---
+++ JAVA_SCALA/Higher_Order_Function/src/Passing_Function_As_Argument.scala
@@ -0,0 +1,18 @@
+
+
+object Passing_Function_As_Argument
+{
+  def main(arg: Array[String])
+  {
+    var sum = (a: Int, b: Int) => a + b
+    var minus = (a: Int, b: Int) => a - b
+    
+    println("Sum of 2 and 3 -> " + calculator(sum, 2, 3))
+    println("Minus of 2 and 3 -> " + calculator(minus, 2, 3))
+  }
+  
+  def calculator(func: (Int, Int) => Int, a: Int, b:Int): Int =
+  {
+    return func(a, b)
+  }
+}
---
+++ JAVA_SCALA/Higher_Order_Function/src/Returning_Function_and_Function_Currying.scala
@@ -0,0 +1,26 @@
+import scala.io.StdIn._
+object Returning_Function
+{
+  def main(arg: Array[String])
+  {
+    println("Enter your first_name, than your last_name: ")
+    
+    //Variable getName will carry a function that is returned from setName() 
+    var getName = setName(readLine(), readLine())
+    
+    println("My name is: " + getName(1))
+  }
+  def setName(first_name: String, last_name: String) = 
+  {
+    //The setName function will return this function to the current variable
+    (num: Int) =>
+      {
+        num match
+        {
+          case 1 => first_name
+          case 2 => last_name
+          case _ => first_name + " " + last_name
+        }
+      }
+  }
+}
