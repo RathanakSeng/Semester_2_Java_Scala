568c3f3d34602ea0b6498deed052bc017c967c29
RathanakSeng <40135662+RathanakSeng@users.noreply.github.com>
2018-10-08 21:20:01 +0700

    Semester

---
+++ JAVA_SCALA/Basic_Scala/src/Operator_Overloading.scala
@@ -0,0 +1,23 @@
+class Student(var name: String, var id: Int)
+{
+  def >(s: Student): Boolean =
+  {
+    return if(id > s.id) true else false
+  }
+  override def toString(): String = 
+  {
+    return id + " : " + name
+  }
+}
+
+
+object Main 
+{
+  def main(arg: Array[String])
+  {
+    var s1 = new Student("Nyratt", 112)
+    var s2 = new Student("Rathanak", 111)
+    
+    if(s1 > s2) println(s2) else println(s1)
+  }
+}
---
+++ JAVA_SCALA/Basic_Scala/src/Pattern_Matching_Demo.scala
@@ -0,0 +1,46 @@
+import scala.io.StdIn._
+
+object Pattern_Matching_Demo
+{
+  def main(arg: Array[String])
+  {
+    print("Enter a number from 1 to 7: ")
+    var num = readInt()
+    
+    num match
+    {
+      case 1 => 
+        {
+          println("Monday")
+        }
+      case 2 =>
+        {
+          println("Tuesday")
+        }
+      case 3 =>
+        {
+          println("Wednesday")
+        }
+      case 4 => 
+        {
+          println("Thursday")
+        }
+      case 5 =>
+        {
+          println("Friday")
+        }
+      case 6 =>
+        {
+          println("Saturday")
+        }
+      case 7 =>
+        {
+          println("Sunday")
+        }
+      case _ =>
+        {
+          println("Dont you read the instrution?? ?")
+        }
+    }
+  }
+}
